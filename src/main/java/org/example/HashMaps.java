39a5a463845d79d9f61253fb38c9ee7d95d380d7
Bekir Can <bcyumrutepe@gmail.com>
2024-02-13 23:20:33 +0300

    Initial commit

---
+++ src/main/java/org/example/HashMaps.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class HashMaps {
+    public HashMaps() {
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, String> renkler = new HashMap();
+        renkler.put("R", "Red");
+        renkler.put("G", "Green");
+        renkler.put("B", "Blue");
+        renkler.put("W", "White");
+        System.out.println((String)renkler.get("R"));
+        System.out.println((String)renkler.get("W"));
+    }
+}
---
+++ src/main/java/org/example/Ogrenci.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Ogrenci {
+    String ad;
+    int yas;
+
+    public Ogrenci(String ad, int yas) {
+        this.ad = ad;
+        this.yas = yas;
+    }
+}
